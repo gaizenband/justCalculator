323e634aa6f50a02932a57bf9ff3a969112a4957
gaizenband <gazik211@gmail.com>
2019-08-13 16:50:48 +0300

    first

---
+++ main.js
@@ -0,0 +1,88 @@
+const calculator = {
+
+    calculation: () => {},
+
+    numbers: () => {}
+}
+
+const main = () => {
+    const actButton = document.querySelector('#allButtons');
+    const input = document.querySelector('#value');
+    const span = document.querySelector('span');
+    const numbers = document.querySelector('#numbers');
+
+    span.innerHTML = 0;
+    let resultStorage = 0;
+    let iterator = 0;
+    let numStorage = '';
+
+
+    calculator.calculation = (e) => {
+        const operation = e.target.dataset.operation;
+        if (!operation) return;       
+        
+
+        switch (operation) {
+            case 'add':
+                resultStorage += +input.value;
+                
+                break;
+            case 'subtract':
+                resultStorage += +input.value;
+                break;
+            case 'multiply':
+                if(!iterator){
+                    resultStorage = +input.value;
+                } else {
+                    resultStorage *= +input.value;
+                }
+                break;
+            case 'divide':
+                if(!iterator){
+                    resultStorage = +input.value;
+                } else {
+                    resultStorage /= +input.value;
+                }
+                break;
+            case 'reset':
+                input.value = '';
+                break;
+        }
+
+        switch (operation) {
+            case 'add':
+            case 'subtract':
+            case 'multiply':
+            case 'divide': 
+                span.innerHTML = +resultStorage;
+                input.value = '';
+                iterator++;
+                numStorage = '';
+                break;
+            case 'reset':
+                span.innerHTML = 0;
+                resultStorage = 0;
+                iterator = 0;
+                numStorage = '';
+        }
+    };
+
+    
+    calculator.numbers = (e) => {
+        const operation = e.target.dataset.operation;
+        if (!operation) return;
+
+        if (operation == 0 && !numStorage) {
+            numStorage = '';
+        } else {
+            numStorage += operation;
+        }    
+    
+        input.value = +numStorage;
+    };
+
+    actButton.addEventListener('click', calculator.calculation);
+    numbers.addEventListener('click', calculator.numbers);
+};
+
+document.addEventListener('DOMContentLoaded', main);
